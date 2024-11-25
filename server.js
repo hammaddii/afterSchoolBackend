1b60381782d539c5868cbf30c541b344e7f3c733
hammaddii <hammadriaz900@gmail.com>
2024-11-25 17:45:57 +0400

    more of the same error

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.set('port', 3000);
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://hammaddii.github.io');
+    res.setHeader('Access-Control-Allow-Origin', 'https://hammaddii.github.io', '*');
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
