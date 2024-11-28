3530fa020823ca2cf4d8e72428b26336ec7225ae
hammaddii <hammadriaz900@gmail.com>
2024-11-28 19:06:03 +0400

    more postman

--- server.js
+++ server.js
@@ -163,6 +163,10 @@ app.put('/postman/collection/clubs/:clubId/updateSpace', async (req, res, next) 
             return res.status(404).send(`Club with ID ${clubId} not found`);
         }
 
+        if (club.availableSpace < spaces) {
+            return res.status(400).send(`Not enough available spaces. Only ${club.availableSpace} spaces left.`);
+        }
+
         const updateResult = await clubsCollection.updateOne(
             { id: parseInt(clubId) },
             { $inc: { availableSpace: -spaces } }
