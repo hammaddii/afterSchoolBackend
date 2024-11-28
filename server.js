ae4e1b137945138f6710f1532c797e12bedeba5d
hammaddii <hammadriaz900@gmail.com>
2024-11-28 18:52:27 +0400

    gave up on postman put

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-const path = require('path');
-const fs = require('fs');
-
-// Logger Middleware
-const loggerMiddleware = (req, res, next) => {
-    const timestamp = new Date().toISOString();
-    console.log(`[${timestamp}] ${req.method} ${req.url}`);
-    next();
-};
 
 app.use(express.json());
 app.set('port', 3000);
-app.use(loggerMiddleware);
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://hammaddii.github.io', '*');
@@ -21,24 +11,6 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
-});
-
-// Static file route to serve images
-const imagesDirectory = path.join(__dirname, 'images');
-app.use('/images', express.static(imagesDirectory));
-
-// Route to handle missing image files
-app.get('/images/:imageName', (req, res, next) => {
-    const imagePath = path.join(imagesDirectory, req.params.imageName);
-
-    // Check if the file exists
-    fs.access(imagePath, fs.constants.F_OK, (err) => {
-        if (err) {
-            return res.status(404).send('Image not found');
-        }
-        // Send the image file if it exists
-        res.sendFile(imagePath);
-    });
 });
 
 let db;
@@ -140,48 +112,27 @@ app.post('/collection/orders', async (req, res, next) => {
     }
 });
 
-// Route to update available space for clubs
 app.put('/collection/clubs/:clubId/updateSpace', async (req, res, next) => {
     if (!db) {
         return res.status(500).send('Database not initialized');
     }
 
-    const clubsCollection = db.collection('clubs');
+    const clubsCollection = db.collection('clubs'); 
     const { clubId } = req.params;
-    const { spaces, type } = req.body;
+    const { spaces } = req.body;
 
-    if (spaces === undefined || spaces < 0) {
-        return res.status(400).send('Invalid number of spaces. Must be a non-negative number.');
+    if (!spaces || spaces <= 0) {
+        return res.status(400).send('Invalid number of spaces. Must be greater than 0.');
     }
 
     try {
-        const club = await clubsCollection.findOne({ id: parseInt(clubId) });
+        // Find and update the club's available space
+        const updateResult = await clubsCollection.updateOne(
+            { id: parseInt(clubId) }, 
+            { $inc: { availableSpace: -spaces } }
+        );
 
-        if (!club) {
-            return res.status(404).send(`Club with ID ${clubId} not found`);
-        }
-
-        let updateResult;
-
-        if (type === "set") {
-            updateResult = await clubsCollection.updateOne(
-                { id: parseInt(clubId) },
-                { $set: { availableSpace: spaces } }
-            );
-        } else if (type === "decrease") {
-            if (spaces > club.availableSpace) {
-                return res.status(400).send('Not enough available space to fulfill this order.');
-            }
-
-            updateResult = await clubsCollection.updateOne(
-                { id: parseInt(clubId) },
-                { $inc: { availableSpace: -spaces } }
-            );
-        } else {
-            return res.status(400).send('Invalid action type. Use "set" or "decrease".');
-        }
-
-        if (updateResult.modifiedCount === 0) {
+        if (updateResult.matchedCount === 0) {
             return res.status(404).send(`Club with ID ${clubId} not found`);
         }
 
